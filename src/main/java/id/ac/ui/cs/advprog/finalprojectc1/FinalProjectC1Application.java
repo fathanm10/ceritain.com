5c86342efe9a6340b1cfb0192b36a31d2d8cfa9f
Dzikri Qalam Hatorangan <dzikri.qalam01@ui.ac.id>
2022-04-13 09:15:17 +0700

    Feat: Setup Initial Repo

---
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/FinalProjectC1Application.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.finalprojectc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectC1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectC1Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/finalprojectc1/FinalProjectC1ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.finalprojectc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectC1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
