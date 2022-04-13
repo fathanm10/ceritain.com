e81fd3a0342bf13fa3a738374e2c0a7c8109af4b
reyzacaloh <reyza.abyan@gmail.com>
2022-04-14 05:27:39 +0700

    add AppUserService

---
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/service/AppUserService.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.finalprojectc1.service;
+
+
+import id.ac.ui.cs.advprog.finalprojectc1.repository.AppUserRepository;
+import id.ac.ui.cs.advprog.finalprojectc1.repository.AppUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("user with email " + email + " not found"));
+
+    }
+}
