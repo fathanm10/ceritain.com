3aeb6d48612cd5790d8275e974d6f25d78b707f1
Rizky Juniastiar <rizky.juniastiar@ui.ac.id>
2022-04-22 17:53:45 +0700

    Add edit_profile.html and edit Profile Page Interface

--- src/main/java/id/ac/ui/cs/advprog/finalprojectc1/controller/ProfileController.java
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/controller/ProfileController.java
@@ -1,18 +1,38 @@
 package id.ac.ui.cs.advprog.finalprojectc1.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path = "/profile")
 public class ProfileController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "")
-    private String home(){
+//    @RequestMapping(method = RequestMethod.GET, value = "")
+//    private String home(){
+//        return "profile";
+//    }
+
+    @GetMapping(value = "")
+    public String profile(Model model){
         return "profile";
     }
+
+    @PostMapping(value = "")
+    public String showProfile(Model model){
+        return "redirect:/profile";
+    }
+
+    @GetMapping(value = "/edit")
+    public String editProfile(Model model){
+        return "edit_profile";
+    }
+
+    @PostMapping(value = "/edit")
+    public String editProfilePost(Model model){
+        return "redirect:/profile/edit";
+    }
+
+
 }
 
