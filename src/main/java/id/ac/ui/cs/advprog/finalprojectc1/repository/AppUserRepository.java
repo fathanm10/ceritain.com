8a0fc3a8d2088c575a1e2f7b102cf320f713a592
Reyza Abyan Gerrit Caloh <reyza.abyan@ui.ac.id>
2022-04-23 18:56:19 +0700

    Revert "add findByName & enableAppUser"
    This reverts commit 00e5011230bcd5870a9a2f131e695bdfea2c9e02

--- src/main/java/id/ac/ui/cs/advprog/finalprojectc1/repository/AppUserRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/repository/AppUserRepository.java
@@ -1,9 +1,8 @@
 package id.ac.ui.cs.advprog.finalprojectc1.repository;
 
 import id.ac.ui.cs.advprog.finalprojectc1.core.appuser.AppUser;
+import id.ac.ui.cs.advprog.finalprojectc1.core.appuser.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,11 +13,4 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface AppUserRepository extends JpaRepository<AppUser,Long> {
     Optional<AppUser> findByEmail(String email);
-    Optional<AppUser> findByName(String name);
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE AppUser a " +
-            "SET a.enabled = TRUE WHERE a.email = ?1")
-    int enableAppUser(String email);
 }
