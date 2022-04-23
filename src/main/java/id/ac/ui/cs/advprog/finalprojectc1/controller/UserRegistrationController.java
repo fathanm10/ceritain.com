71e0f139f62673f457c3f56dffa096bda0dd3df2
reyzacaloh <reyza.abyan@gmail.com>
2022-04-23 11:05:56 +0700

    add confirm token handler & landing page for successfully logged user

--- src/main/java/id/ac/ui/cs/advprog/finalprojectc1/controller/UserRegistrationController.java
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/controller/UserRegistrationController.java
@@ -3,10 +3,7 @@ package id.ac.ui.cs.advprog.finalprojectc1.controller;
 import id.ac.ui.cs.advprog.finalprojectc1.core.registration.RegistrationRequest;
 import id.ac.ui.cs.advprog.finalprojectc1.service.RegistrationService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "cerita/registration")
@@ -19,5 +16,17 @@ public class UserRegistrationController {
     public String register (@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
+
+    @GetMapping(path = "/confirm/{token}")
+    public String confirm(@PathVariable(value = "token") String token){
+        return registrationService.confirmToken(token);
+    }
+
+    @GetMapping( path = "/logged")
+    public String logged() {
+        return "Login successful";
+    }
+
+
 }
  
