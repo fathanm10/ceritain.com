a5549317d887c12db0abb63ce57026a5036e4217
Rizky Juniastiar <rizky.juniastiar@ui.ac.id>
2022-05-13 23:56:35 +0700

    Integrating Profile user to backend

--- src/main/java/id/ac/ui/cs/advprog/finalprojectc1/controller/UserProfileController.java
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/controller/UserProfileController.java
@@ -1,5 +1,10 @@
 package id.ac.ui.cs.advprog.finalprojectc1.controller;
 
+import id.ac.ui.cs.advprog.finalprojectc1.model.Profile;
+import id.ac.ui.cs.advprog.finalprojectc1.repository.AppUserRepository;
+import id.ac.ui.cs.advprog.finalprojectc1.repository.ProfileRepository;
+import id.ac.ui.cs.advprog.finalprojectc1.service.ProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -8,24 +13,46 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/profile")
 public class UserProfileController {
 
+    private static final String PROFILE = "profile";
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private ProfileService profileService;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
     @GetMapping(value = "")
-    public String profile(Model model){
-        return "profile";
+    public String homepage(Model model){
+
+        return PROFILE;
+    }
+
+    @GetMapping(value = "/{id}")
+    public String profilePage(@PathVariable int id, Model model){
+        model.addAttribute("user", profileService.getProfileById(id));
+        return PROFILE;
     }
 
     @PostMapping(value = "")
-    public String showProfile(Model model){
-        return "redirect:/profile";
+    public String createProfile(Model model){
+        return "redirect:/profile/edit";
     }
 
     @GetMapping(value = "/edit")
     public String editProfile(Model model){
+        model.addAttribute(PROFILE, new Profile());
         return "edit_profile";
     }
 
     @PostMapping(value = "/edit")
-    public String editProfilePost(Model model){
-        return "redirect:/profile/edit";
+    public String editProfilePost(@RequestParam(value = "linkPhoto") String linkPhoto, @RequestParam(value = "firstName") String firstName,
+                              @RequestParam(value = "lastName") String lastName, @RequestParam(value = "email") String email,
+                              @RequestParam(value = "username") String username, @RequestParam(value = "bio") String bio){
+        int id = profileService.saveProfileToDB(firstName, lastName, email, username, bio, linkPhoto);
+        return "redirect:/profile/" + id;
     }
 
 
