996425b6b50d0135e5e006462d9e0449819624a0
anindyasasriya <anindya.sasriya@ui.ac.id>
2022-05-14 01:54:21 +0700

    Add search page repository

---
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/model/Cerita.java
@@ -0,0 +1,41 @@
+package id.ac.ui.cs.advprog.finalprojectc1.model;
+
+/*import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="cerita")
+*/public class Cerita { /*
+
+    @Id
+    @Column(name="cerita_id")
+    @GeneratedValue(generator="uuid2")
+    @GenericGenerator(name="uuid2", strategy= "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @Column(name="cerita_judul")
+    private String judulCerita;
+
+    @Column(name="cerita_isi")
+    private String isiCerita;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+*/
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/repository/SearchPageRepository.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.finalprojectc1.repository;
+
+import id.ac.ui.cs.advprog.finalprojectc1.model.Cerita;
+/*import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+public interface SearchPageRepository  extends JpaRepository<Cerita, String> {
+    @Query("SELECT c FROM cerita c")
+    public List<Cerita> search(String keyword);
+
+    public Cerita findByName(String name);
+
+}*/
