567445048ff5bd0addaa9582535d6b041291ade5
Rizky Juniastiar <rizky.juniastiar@ui.ac.id>
2022-06-11 23:33:59 +0700

    Remove code smell on MainController

--- src/main/java/id/ac/ui/cs/advprog/finalprojectc1/controller/MainController.java
+++ src/main/java/id/ac/ui/cs/advprog/finalprojectc1/controller/MainController.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.finalprojectc1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
